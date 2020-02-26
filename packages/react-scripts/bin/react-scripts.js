0aaae6f8e5f5d7975204a3b9aa15e95ba374bc02
hisa <hisa@iporaitech.com>
2020-02-26 11:16:04 -0300

    Sync to react-scripts@3.4.0

--- packages/react-scripts/bin/react-scripts.js
+++ packages/react-scripts/bin/react-scripts.js
@@ -21,10 +21,10 @@ const args = process.argv.slice(2);
 const scriptIndex = args.findIndex(
   x =>
     x === 'build' ||
-    x === 'build-custom' ||
     x === 'eject' ||
     x === 'start' ||
-    x === 'test'
+    x === 'test' ||
+    x === 'build-custom'
 );
 const script = scriptIndex === -1 ? args[0] : args[scriptIndex];
 const nodeArgs = scriptIndex > 0 ? args.slice(0, scriptIndex) : [];
